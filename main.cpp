7dff60d1c5f676dc925dd440dcc106b50ba8a662
virginia5533 <vlv5@students.uwf.edu>
2021-11-16 14:15:26 -0600

    main

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//Virginia Vaughan
+//Main.cpp
+
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+/*
+ *Prompts user for input, stores cylinders into vector, and contains switch case to decide function
+  Scan, C-Scan, or Quit
+ * */
+
+
+int main () {
+
+	vector<int> cylinders;
+
+	const int MAX = 299;
+	const int MIN = 0;
+
+	cout << "This disk scheduler tries to optimize on seek time for disk accesses." << endl;
+
+
+
+return 0;
+}
