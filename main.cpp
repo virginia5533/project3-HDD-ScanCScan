026e3da35c9c0d59c13f645f1c6f7007512bf210
virginia5533 <vlv5@students.uwf.edu>
2021-11-16 14:23:32 -0600

    basic ints assigned

--- main.cpp
+++ main.cpp
@@ -26,7 +26,26 @@ int main () {
 	const int MAX = 299;
 	const int MIN = 0;
 
+	int currentLoc, travelDir, flyback;
+
+
+
 	cout << "This disk scheduler tries to optimize on seek time for disk accesses." << endl;
+	
+	cout << "Please enter the current location of the arm: ";
+	cin >> currentLoc;
+	cout << endl;
+
+	cout << "Please enter the travel direction of the arm." << endl
+		<< "(1: Going towards higher numbers, 2: Going towards lower numbers): ";
+	cin >> travelDir;
+	cout << endl;
+
+	cout << "Please enter the flyback time in number of cylinders: ";
+	cin >> flyback;
+	cout << endl;
+
+	cout << "Please enter the list of cylinder numbers for the file: ";
 
 
 
